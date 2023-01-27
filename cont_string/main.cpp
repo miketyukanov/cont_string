b415235cbb7e9c350b00b1dece211707dbf7942b
Mike Tyukanov <miketyukanov@gmail.com>
2023-01-27 18:46:11 +0300

    Initial Commit

---
+++ cont_string/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cont_string
+//
+//  Created by Mike Tyukanov on 27.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
