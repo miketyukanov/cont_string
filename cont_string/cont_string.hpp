4d7102e5c7c80dbb5e9248e40e947f0822963d74
Mike Tyukanov <miketyukanov@gmail.com>
2023-01-29 16:31:11 +0300

    The header for container-string tests

---
+++ cont_string/cont_string.hpp
@@ -0,0 +1,13 @@
+//
+//  cont_string.hpp
+//  cont_string
+//
+//  Created by Mike Tyukanov on 29.01.2023.
+//
+
+#ifndef cont_string_hpp
+#define cont_string_hpp
+
+void cont_string_test(void);
+
+#endif /* cont_string_hpp */
